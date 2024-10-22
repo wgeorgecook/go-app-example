e6e827c31fe9c79885728d47e6e165f71331a12a
William Cook <=>
2024-10-21 21:17:11 -0400

    Scaffold database interactions

---
+++ data-access/internal/pkg/db/schema.go
@@ -0,0 +1,38 @@
+package db
+
+import "time"
+
+// diet is the database representation
+// the a petsapisv1.Diet
+type diet int32
+
+const (
+	DIET_UNSPECIFIED diet = iota
+	DIET_NO_RESTRICTIONS
+	DIET_GRAIN_FREE
+	DIET_RAW
+)
+
+// friendlyWith is the database representation
+// the a petsapisv1.FriendlyWith
+type friendlyWith int32
+
+const (
+	FRIENDLY_WITH_UNSPECIFIED friendlyWith = iota
+	FRIENDLY_WITH_LITTLE_DOGS
+	FRIENDLY_WITH_BIG_DOGS
+	FRIENDLY_WITH_CATS
+	FRIENDLY_WITH_YOUNG_CHILDREN
+	FRIENDLY_WITH_OLDER_CHILDREN
+)
+
+// Pet is the database representation of a
+// petapisv1.Pet.
+type Pet struct {
+	Name         string
+	Birthdate    time.Time
+	Description  string
+	Diet         diet
+	FriendlyWith friendlyWith
+	PictureUrl   string
+}
