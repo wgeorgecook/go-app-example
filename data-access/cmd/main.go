f5fe9e02c707bed1024eb63f34f55184bdfa1dde
William Cook <=>
2024-10-22 21:42:26 -0400

    Run migrations on app start

--- data-access/cmd/main.go
+++ data-access/cmd/main.go
@@ -1,6 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/wgeorgecook/go-app-example/data-access/internal/pkg/db"
+)
+
+func init() {
+	if err := db.Migrate(); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	log.Println("Hello World! This is the data access layer.")
