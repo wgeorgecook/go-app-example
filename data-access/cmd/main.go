b116a0657d7a737510bb668515bd35b61b7ffefb
William Cook <=>
2024-10-23 23:03:18 -0400

    Graceful shutdown

--- data-access/cmd/main.go
+++ data-access/cmd/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
-	petapisv1 "buf.build/gen/go/wgeorgecook/go-app-example/protocolbuffers/go/wgeorgecook/petapis/v1"
 	"github.com/wgeorgecook/go-app-example/data-access/internal/pkg/db"
+	"github.com/wgeorgecook/go-app-example/data-access/server"
 )
 
 const POSTGRES_URI = "postgres://postgres:postgres@postgres:5432/pets?sslmode=disable"
+const GRPC_PORT = "8088"
 
 func init() {
 	if err := db.Migrate(POSTGRES_URI); err != nil {
@@ -18,14 +23,22 @@ func init() {
 func main() {
 	log.Println("Hello World! This is the data access layer.")
 
-	conn, err := db.Connect(POSTGRES_URI)
-	if err != nil {
-		panic(err)
-	}
-	pets, err := db.Query(conn, []petapisv1.Query{})
-	if err != nil {
+	if err := db.Connect(POSTGRES_URI); err != nil {
 		panic(err)
 	}
 
-	log.Printf("pets: %+v\n", pets)
+	ctx := context.Background()
+	go server.Start(GRPC_PORT)
+
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
+	log.Println("server is running, CTRL + C to shutdown")
+	<-exit
+	log.Println("shutdown received")
+	if err := db.Shutdown(ctx); err != nil {
+		log.Println("could not close db connection: ", err.Error())
+	}
+
+	server.Shutdown()
+	log.Println("shutdown complete")
 }
