a7604a055fedc77e91a6ffa8d26524c56f642782
William Cook <=>
2024-10-23 23:04:11 -0400

    Proper server receiver

--- data-access/server/handlers.go
+++ data-access/server/handlers.go
@@ -7,7 +7,7 @@ import (
 	petapisv1 "buf.build/gen/go/wgeorgecook/go-app-example/protocolbuffers/go/wgeorgecook/petapis/v1"
 )
 
-func (petServiceServer) GetPets(ctx context.Context, req *petapisv1.GetPetsRequest) (*petapisv1.GetPetsResponse, error) {
+func (s *petServiceServer) GetPets(ctx context.Context, req *petapisv1.GetPetsRequest) (*petapisv1.GetPetsResponse, error) {
 	log.Println("start get pets")
 	defer log.Println("get pets complete")
 	return nil, nil
